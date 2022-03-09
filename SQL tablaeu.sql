5319450ce82bc4cb09164aca7883cce66f6af671
Ankush11194 <65784089+Ankush11194@users.noreply.github.com>
2022-03-09 11:52:22 +0530

    Update SQL tablaeu.sql

--- SQL tablaeu.sql
+++ SQL tablaeu.sql
@@ -1,3 +1,12 @@
+/*
+Queries used for Tableau Visualization
+*/
+
+
+/*
+This query will tell us about the total cases, total death and the deathpercentage due to covid
+*/
+
 select sum(new_cases) as total_cases, sum(cast(new_deaths as bigint)) as total_deaths,
 sum(cast(new_deaths as bigint))/sum(new_cases)*100 as Deathpercentage
 from PortfolioProject..CovidDeaths
@@ -6,13 +15,21 @@ where continent is not null
 --group by date
 order by 1,2
 
+/*
+ This query will give all continent total death counts
+ */
+ 
 select location, sum(cast(new_deaths as bigint)) as TotalDeathcount
 from PortfolioProject..CovidDeaths
 --where location like '%states%'
 where continent is null
-and location not in ('World', 'European Union','International','Upper middle income','High income', 'Lower middle income')
+and location not in ('World', 'European Union','International','Upper middle income','High income', 'Lower middle income', 'Low income')
 group by location
 order by TotalDeathcount desc
+
+/*
+This query will give the population count in each countries and their highest infection count and percentage population infected
+*/
 
 select location, Population, max(total_cases) as HighestInfectionCount, 
 max(total_cases/population)*100 as PercentPopulationInfected
@@ -20,6 +37,10 @@ from PortfolioProject..CovidDeaths
 --where location ='india'
 group by location, population
 order by PercentPopulationInfected desc
+
+/*
+This query is same as above just we have return the date
+*/
 
 select location, population, date,max(total_cases) as HighestInfectionCount,
 max((total_cases/population))*100 as PercentPopulationInfected
